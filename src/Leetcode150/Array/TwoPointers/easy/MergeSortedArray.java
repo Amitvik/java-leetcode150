f46e7f28b37aadfc7b6f4de059439781990d9fb9
AmitHeelDev <amit@theheel.com>
2025-06-24 21:19:48 -0500

    -Solved LC:80 Remove Duplicates from Sorted Array II

--- src/Leetcode150/Array/TwoPointers/easy/MergeSortedArray.java
+++ src/Leetcode150/Array/TwoPointers/easy/MergeSortedArray.java
@@ -1,4 +1,4 @@
-package Leetcode150.Array.TwoPointers;
+package Leetcode150.Array.TwoPointers.easy;
 
 /*
 You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
